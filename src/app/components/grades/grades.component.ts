f90b5b2e0e29a30375d024dfdf4e1013b9e2b9a1
brionnevi <victor.brionne@gmail.com>
2022-03-08 23:03:19 +0100

    Grade conditionnal color

--- src/app/components/grades/grades.component.ts
+++ src/app/components/grades/grades.component.ts
@@ -1,3 +1,4 @@
+import { NgStyle } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Grade } from 'src/app/services/models/Grade';
 import { TeachingUnit } from 'src/app/services/models/TeachingUnit';
@@ -14,29 +15,36 @@ export class GradesComponent {
   constructor() {
     this.teachingUnits = [
       { name: "Sciences appliquées", grades: [
+          { label: "Analyse", value: 5 },
+          { label: "Analyse", value: 10 },
           { label: "Analyse", value: 17 },
-          { label: "Analyse", value: 17 },
-          { label: "Analyse", value: 17 },
-        ]
-      },
-      { name: "Sciences appliquées", grades: [
           { label: "Analyse", value: 17 },
           { label: "Analyse", value: 17 },
           { label: "Analyse", value: 17 },
         ]
       },
-      { name: "Sciences appliquées", grades: [
-          { label: "Analyse", value: 17 },
-          { label: "Analyse", value: 17 },
-          { label: "Analyse", value: 17 },
+      { name: "Informatique", grades: [
+          { label: "Analyse", value: 9 },
+          { label: "Analyse", value: 20 },
+          { label: "Analyse", value: 14 },
         ]
       },
-      { name: "Sciences appliquées", grades: [
-          { label: "Analyse", value: 17 },
-          { label: "Analyse", value: 17 },
-          { label: "Analyse", value: 17 },
+      { name: "Humanités", grades: [
+          { label: "Analyse", value: 15 },
+          { label: "Analyse", value: 10.5 },
+          { label: "Analyse", value: 12 },
         ]
-      },
+      }
     ];
+  }
+
+  public getGradeColor(grade: Grade): string {
+    if (grade.value < 10) {
+      return 'red';
+    } else if (grade.value == 10) {
+      return 'orange'
+    } else {
+      return 'green';
+    }
   }
 }
